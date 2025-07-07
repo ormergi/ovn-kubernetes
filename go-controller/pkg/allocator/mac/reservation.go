74d92bc235db21376954bddf50978c6d464644fd
Ram Lavi <ralavi@redhat.com>
2025-07-07 11:09:18 +0300

    pod-allocator: Introduce reserved MAC addresses manager for MAC detection
    Adds a thread-safe manager enable tracking used MAC addresses
    per network with owner identification.
    Enabling MAC addresses conflict detection when multiple entities try
    to use the same resources within the same network.
    
    The network pool will be integrated into cluster-manager's
    pod allocator code in follow-up commits.
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>
    Co-authored-by: Ram Lavi <ralavi@redhat.com>

---
+++ go-controller/pkg/allocator/mac/reservation.go
@@ -0,0 +1,89 @@
+package mac
+
+import (
+	"errors"
+	"net"
+	"sync"
+
+	"k8s.io/klog/v2"
+)
+
+// store for reserved MAC address request by owner. Key is MAC address, value is owner identifier.
+type store map[string]string
+
+// ReservationManager tracks reserved MAC addresses requests of pods on user-defined networks and detect MAC conflicts,
+// where one pod request static MAC address that is used by another pod.
+// Isolation is maintained, the same MAC address can be used in multiple user-defined networks.
+type ReservationManager struct {
+	// lock for storing a MAC reservation.
+	lock sync.Mutex
+	// networkReservations store for user-defined networks MAC reservations. key is network name, value is store.
+	networkReservations map[string]store
+}
+
+// NewManager creates a new ReservationManager.
+func NewManager() *ReservationManager {
+	return &ReservationManager{
+		networkReservations: make(map[string]store),
+	}
+}
+
+var ErrMACConflict = errors.New("MAC address already in use")
+
+// Reserve stores the address reservation and its owner in the given network store.
+// Returns an error ErrMACConflict in case the given addresses is already reserved on the network by different owner.
+func (n *ReservationManager) Reserve(network, owner string, mac net.HardwareAddr) error {
+	if network == "" || owner == "" || mac == nil {
+		return nil
+	}
+
+	n.lock.Lock()
+	defer n.lock.Unlock()
+
+	macStore, exists := n.networkReservations[network]
+	if !exists {
+		macStore = make(store)
+		n.networkReservations[network] = macStore
+		klog.V(5).Infof("Created MAC reserveation store for network: %s", network)
+	}
+
+	macKey := mac.String()
+	if currentOwner, macReserved := macStore[macKey]; macReserved && currentOwner != owner {
+		return ErrMACConflict
+	}
+
+	n.networkReservations[network][macKey] = owner
+	klog.V(5).Infof("Reserved MAC (%s) for owner (%s) on network (%s)", macKey, owner, network)
+
+	return nil
+}
+
+// Release removes MAC address store from given network store.
+func (n *ReservationManager) Release(network string, owner string, mac net.HardwareAddr) error {
+	if network == "" || owner == "" || mac == nil {
+		return nil
+	}
+
+	n.lock.Lock()
+	defer n.lock.Unlock()
+
+	macStore, exists := n.networkReservations[network]
+	if !exists {
+		return nil
+	}
+
+	macKey := mac.String()
+	if currentOwner, exists := macStore[macKey]; exists && currentOwner != owner {
+		return nil
+	}
+
+	delete(macStore, macKey)
+	klog.V(5).Infof("Release MAC (%s) of owner (%s) on network (%s)", macKey, owner, network)
+
+	if len(macStore) == 0 {
+		delete(n.networkReservations, network)
+		klog.V(5).Infof("Deleted network %s MAC reserveation store", network)
+	}
+
+	return nil
+}
---
+++ go-controller/pkg/allocator/mac/reservation_test.go
@@ -0,0 +1,91 @@
+package mac_test
+
+import (
+	"errors"
+	"net"
+
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/allocator/mac"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ReservationManager", func() {
+	const testNetwork = "test-network"
+	const owner1 = "namespace1/pod1"
+	const owner2 = "namespace2/pod2"
+
+	var tracker *mac.ReservationManager
+	var mac1, mac2 net.HardwareAddr
+
+	BeforeEach(func() {
+		var err error
+		mac1, err = net.ParseMAC("aa:bb:cc:dd:ee:f1")
+		Expect(err).NotTo(HaveOccurred())
+		mac2, err = net.ParseMAC("aa:bb:cc:dd:ee:f2")
+		Expect(err).NotTo(HaveOccurred())
+		tracker = mac.NewManager()
+	})
+
+	Context("reserve", func() {
+		DescribeTable("should handle given zero value gracefully",
+			func(network, owner string, mac net.HardwareAddr) {
+				Expect(tracker.Reserve(network, owner, mac)).To(Succeed())
+			},
+			Entry("", "", "", nil),
+			Entry("", testNetwork, "", nil),
+			Entry("", "", owner1, nil),
+			Entry("", "", "", mac1),
+			Entry("", testNetwork, owner1, nil),
+			Entry("", testNetwork, "", mac1),
+			Entry("", "", owner1, mac1),
+		)
+		It("should allow adding same MAC multiple times without duplicates", func() {
+			Expect(tracker.Reserve(testNetwork, owner1, mac1)).To(Succeed())
+		})
+		It("should not fail on repeated reservation", func() {
+			Expect(tracker.Reserve(testNetwork, owner1, mac1)).To(Succeed())
+			Expect(tracker.Reserve(testNetwork, owner1, mac1)).To(Succeed(), "same owner should not raise a conflict")
+			err := tracker.Reserve(testNetwork, owner2, mac1)
+			Expect(err).NotTo(HaveOccurred(), "diffrent owner should not raise a conflict")
+			Expect(errors.Is(err, mac.ErrMACConflict)).To(BeTrue())
+		})
+		It("should isolate MACs between different networks", func() {
+			const anotherNetwork = "another-network"
+
+			Expect(tracker.Reserve(testNetwork, owner1, mac1)).To(Succeed())
+			Expect(tracker.Reserve(anotherNetwork, owner1, mac2)).To(Succeed())
+
+			// MAC1 should conflict only in test-network
+			Expect(tracker.Reserve(testNetwork, owner2, mac1)).ToNot(Succeed(), "mac1 already reserved in testNetwork for owner1")
+			Expect(tracker.Reserve(anotherNetwork, owner2, mac1)).To(Succeed(), "mac1 not reserved in anotherNetwork")
+			// MAC2 should conflict only in another-network
+			Expect(tracker.Reserve(testNetwork, owner2, mac2)).To(Succeed(), "mac2 not reserved ion testNetwork")
+			Expect(tracker.Reserve(anotherNetwork, owner2, mac2)).ToNot(Succeed(), "mac2 already reserved in anotherNetwork for owner2")
+		})
+	})
+
+	Context("release", func() {
+		DescribeTable("should handle given zero values gracefully",
+			func(network, owner string, mac net.HardwareAddr) {
+				Expect(tracker.Release(network, owner, mac)).To(Succeed())
+			},
+			Entry("", "", "", nil),
+			Entry("", testNetwork, "", nil),
+			Entry("", "", owner1, nil),
+			Entry("", "", "", mac1),
+			Entry("", testNetwork, owner1, nil),
+			Entry("", testNetwork, "", mac1),
+			Entry("", "", owner1, mac1),
+		)
+
+		It("should successfully remove existing MAC", func() {
+			Expect(tracker.Reserve(testNetwork, owner1, mac1)).To(Succeed(), "should reserve mac1 for owner1")
+			Expect(tracker.Reserve(testNetwork, owner2, mac1)).ToNot(Succeed(), "should fail because mac1 should be reserved by owner1")
+
+			Expect(tracker.Release(testNetwork, owner1, mac1)).To(Succeed(), "should release mac1")
+
+			Expect(tracker.Reserve(testNetwork, owner2, mac1)).To(Succeed(), "should reserved mac1 as it should be free")
+		})
+	})
+})
