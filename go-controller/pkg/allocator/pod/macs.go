5306fe49d4dd35d57f63c78f64f47037d9b04814
Or Mergi <ormergi@redhat.com>
2025-08-18 21:12:51 +0300

    clustermanager,allocator: Init network pool with infra MACs
    When EnablePreconfiguredUDNAddresses is enabled, initialize the MAC
    manager store with MACs of the network GW and management ports to
    prevent conflicts with pods requesting those MACs.
    
    The network logical switch has GW (.1) and management (.2) ports.
    Their MAC address is generated from the IP address.
    Calculate the GW and management MAC addresses from their IP addresses.
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>
    Co-authored-by: Ram Lavi <ralavi@redhat.com>

--- go-controller/pkg/allocator/pod/macs.go
+++ go-controller/pkg/allocator/pod/macs.go
@@ -7,7 +7,10 @@ import (
 	kubevirtv1 "kubevirt.io/api/core/v1"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/klog/v2"
+	k8snet "k8s.io/utils/net"
 
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
 )
 
@@ -51,4 +54,42 @@ func (allocator *PodAnnotationAllocator) ReleasePodReservedMacAddress(pod *corev
 	}
 
 	return nil
+}
+
+// InitializeMACManager initializes MAC reservation tracker with MAC addresses in use in the network.
+func (allocator *PodAnnotationAllocator) InitializeMACManager() error {
+	macs := calculateSubnetsInfraMACAddresses(allocator.netInfo.Subnets())
+
+	networkName := allocator.netInfo.GetNetworkName()
+	for owner, mac := range macs {
+		if rerr := allocator.macManager.Reserve(networkName, owner, mac); rerr != nil {
+			return fmt.Errorf("failed to reserve MAC (%s) for owner (%s) on network (%s): %w",
+				mac, owner, networkName, rerr)
+		}
+	}
+
+	return nil
+}
+
+// calculateSubnetsInfraMACAddresses return map of the network infrastructure mac addresses and owner name.
+// It calculates the gateway (.2) and management (.1) ports MAC addresses from their IP address.
+func calculateSubnetsInfraMACAddresses(subnets []config.CIDRNetworkEntry) map[string]net.HardwareAddr {
+	reservedMACs := map[string]net.HardwareAddr{}
+	for _, subnet := range subnets {
+		if subnet.CIDR == nil {
+			continue
+		}
+
+		gwIP := util.GetNodeGatewayIfAddr(subnet.CIDR)
+		gwMAC := util.IPAddrToHWAddr(gwIP.IP)
+		gwKey := fmt.Sprintf("gw-v%s", k8snet.IPFamilyOf(gwIP.IP))
+		reservedMACs[gwKey] = gwMAC
+
+		mgmtIP := util.GetNodeManagementIfAddr(subnet.CIDR)
+		mgmtMAC := util.IPAddrToHWAddr(mgmtIP.IP)
+		mgmtKey := fmt.Sprintf("mgmt-v%s", k8snet.IPFamilyOf(mgmtIP.IP))
+		reservedMACs[mgmtKey] = mgmtMAC
+	}
+
+	return reservedMACs
 }
--- go-controller/pkg/clustermanager/network_cluster_controller.go
+++ go-controller/pkg/clustermanager/network_cluster_controller.go
@@ -246,6 +246,14 @@ func (ncc *networkClusterController) init() error {
 			ipamClaimsReconciler,
 		)
 
+		if config.OVNKubernetesFeature.EnablePreconfiguredUDNAddresses &&
+			ncc.IsPrimaryNetwork() &&
+			ncc.TopologyType() == types.Layer2Topology {
+			if err := podAllocationAnnotator.InitializeMACManager(); err != nil {
+				return fmt.Errorf("failed to intialize MAC addresses manager: %w", err)
+			}
+		}
+
 		ncc.podAllocator = pod.NewPodAllocator(
 			ncc.GetNetInfo(),
 			podAllocationAnnotator,
